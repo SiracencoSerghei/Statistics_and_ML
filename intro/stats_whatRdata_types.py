ccce95fb9132989313b72fcce4d87826c034ae8a
Siracenco Serghei <siracencoserghei@email.com>
2024-09-04 11:39:06 +0200

    whatRdata

--- intro/stats_whatRdata_types.py
+++ intro/stats_whatRdata_types.py
@@ -4,13 +4,10 @@
 # What are (is?) data?
 # Code: Representing types of data on computers
 
-# In[ ]:
-
-
-## create variables of different types (classes)
+# Create variables of different types (classes)
 
 # data numerical (here as a list)
-numdata = [ 1, 7, 17, 1717 ]
+numdata = [1, 7, 17, 1717]
 
 # character / string
 chardata = 'xyz'
@@ -19,34 +16,35 @@ chardata = 'xyz'
 strdata = "x"
 
 # boolean (aka logical)
-logitdata = True # notice capitalization!
+logitdata = True  # notice capitalization!
 
 # a list can be used like a MATLAB cell
-listdata = [ [3, 4, 34] , 'hello' , 4 ]
+listdata = [[3, 4, 34], 'hello', 4]
 
-# dict (kindof similar to MATLAB structure)
-dictdata = dict()
-dictdata['name'] = 'Mike'
-dictdata['age'] = 25
-dictdata['occupation'] = 'Nerdoscientist'
+# dict (kind of similar to MATLAB structure)
+dictdata = {
+    'name': 'Mike',
+    'age': 25,
+    'occupation': 'Nerdoscientist'
+}
 
-
-# In[ ]:
-
-
-# let's see what the workspace looks like
-get_ipython().run_line_magic('whos', '')
-
-
-# In[ ]:
-
-
-# clear the Python workspace
-get_ipython().run_line_magic('reset', '-sf')
+# Inspect the variables (this will print them)
+print("Numerical data:", numdata)
+print("Character data:", chardata)
+print("String data:", strdata)
+print("Boolean data:", logitdata)
+print("List data:", listdata)
+print("Dictionary data:", dictdata)
 
 
-# In[ ]:
+# Clear the Python workspace (not usually recommended to do this in a script)
+# To clear variables in a script, you can manually delete them
+del numdata, chardata, strdata, logitdata, listdata, dictdata
 
-
+# Check if variables are cleared (this will raise a NameError if they are cleared)
+try:
+    print(numdata)
+except NameError:
+    print("Variables have been cleared.")
 
 
